12a7951d4cfaf9e6a4beeca9db4a994b3571eed3
Chan, PoChih <PoChih.Chan@Emerson.com>
2022-05-10 14:50:39 +0800

    Add Questions.Item_3_1 & Questions.Item_3_2.

---
+++ InterviewExam/InterviewExam/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace InterviewExam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Question 0:
+            // Please create a new branch based on the main branch for your interview exams. The new branch name is: candidate/{your name}.
+
+            Questions questions = new Questions();
+            // Question 1:
+            // Please complete the logic of Item_3_1
+            questions.Item_3_1();
+
+            // Question 2:
+            // Please complete the logic of Item_3_2
+            questions.Item_3_2();
+        }
+    }
+}
---
+++ InterviewExam/InterviewExam/Questions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewExam
+{
+    public class Questions
+    {
+        /// <summary>
+        /// Write a function to print numbers that are not duplicated in an Integer array.
+        /// Input: [1, 5, 11, 2, 4, 9, 4, 1]
+        /// Output: [5, 11, 2, 9]
+        /// </summary>
+        public void Item_3_1(int[] input = null)
+        {
+            if (input == null)
+            {
+                input = new int[] { 1, 5, 11, 2, 4, 9, 4, 1 };
+            }
+
+            Console.WriteLine("Answer: ");
+        }
+
+        /// <summary>
+        /// Write a function to find missing numbers in a continuous numerical series.
+        /// Input: [3, 6, 9, 7, 4]
+        /// Output: [5, 8]
+        /// </summary>
+        public void Item_3_2(int[] input = null)
+        {
+            if (input == null)
+            {
+                input = new int[] { 3, 6, 9, 7, 4 };
+            }
+            Console.WriteLine("Answer: ");
+        }
+    }
+}
